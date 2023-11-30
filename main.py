c1f478d857c1f5d694287e29144ad986e02796c3
headhunter_jr <mykola.anchiforov@lnu.edu.ua>
2023-11-30 16:46:24 +0200

    30.11.2023 pie chart by departments

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from worker import *
 from database import WorkerDatabase
+import pandas as pd
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    db = WorkerDatabase("text.csv")
-    db.plot_future_salary_increase(1, 24)
-    db.plot_future_salary_increase(4, 24)
-    db.plot_future_salary_increase(5, 24)
+    # db = WorkerDatabase("text.csv")
+    # db.plot_future_salary_increase(1, 24)
+    # db.plot_future_salary_increase(4, 24)
+    # db.plot_future_salary_increase(5, 24)
+    dataframe = pd.read_csv('text.csv')
+    departments = dataframe['department'].value_counts()
+    departments.plot.pie(autopct='%1.1f%%', startangle=90)
+    plt.show()
